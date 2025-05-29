978ca7b5466ffeaf5f6355d04f51b3c4d03a8471
LBF38 <contact.lbf38@gmail.com>
2025-05-29 12:46:34 +0200

    feat: :sparkles: implement DFS on Adjacency List

---
+++ src/day1/DFSGraphList.ts
@@ -0,0 +1,47 @@
+export default function dfs(
+    graph: WeightedAdjacencyList,
+    source: number,
+    needle: number,
+): number[] | null {
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    const path: number[] = [];
+
+    walk(graph, source, needle, seen, path);
+    if (path.length === 0) {
+        return null;
+    }
+    return path;
+}
+
+function walk(
+    graph: WeightedAdjacencyList,
+    current: number,
+    needle: number,
+    seen: boolean[],
+    path: number[],
+): boolean {
+    // base cases
+    if (seen[current]) return false;
+
+    seen[current] = true;
+
+    // recurse steps
+
+    // pre
+    path.push(current);
+    if (current === needle) return true;
+
+    // recurse
+    const list = graph[current];
+    for (let i = 0; i < list.length; i++) {
+        const edge = list[i];
+        if (walk(graph, edge.to, needle, seen, path)) {
+            return true;
+        }
+    }
+
+    // post
+    path.pop();
+
+    return false;
+}
